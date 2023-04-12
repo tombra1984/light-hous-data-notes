50c049e1a20a662ca03fafa9d754cfc78a2a1dfa
tombra1984 <brossatombra@gmail.com>
2023-04-12 12:19:18 -0500

    push 3

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+I used to be a farmer, and I made a fine living
+I had a little stretch of land along the CP line
+But times were hard and though I tried, the money wasn't there
+And the bankers came and took my land and told me "fair is fair".
+
+I looked for every kind of job, and the answer was always no
+"Hire you right now?" they'd always laugh, "We just let twenty go!"
+The government, they promised me a measly little sum
+But I've got too much pride to end up on the streets.
+
+Then I thought, who gives a damn if all the jobs are gone?
+I'm gonna be a PIRATE on the river of Saskatchewan!
